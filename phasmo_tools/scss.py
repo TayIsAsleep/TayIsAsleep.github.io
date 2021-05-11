118b4b6902043f7a7843ec2bd3a564ed13a00cfe
TayIsAsleep <drpeperoni@outlook.com>
2021-05-12 00:03:26 +0200

    Added minimizing CSS into the chain

--- phasmo_tools/scss.py
+++ phasmo_tools/scss.py
@@ -1,10 +1,19 @@
-import requests
+from requests import post
+from time import sleep
 
-with open("static/css/style.scss", "r", encoding="utf-8") as in_file:
-    with open("static/css/style.css", "w", encoding="utf-8") as out_file:
-        out_file.write(
-            requests.post(
-                url='https://jsonformatter.org/service/scssTocss',
-                data={'css':in_file.read()}
-            ).text
-        )
+with open("static/css/style.scss", "r", encoding="utf-8") as in_file, open("static/css/style.css", "w", encoding="utf-8") as out_file:
+    print("Reading from .scss file...")
+    scss_data = in_file.read()
+
+    print("Converting scss to css...")
+    css_data = post(url='https://jsonformatter.org/service/scssTocss', data={'css':scss_data}).text
+    
+    print("Minimizing css...")
+    minimized_css_data = post(url='https://cssminifier.com/raw', data={'input':css_data}).text
+    
+    print("Writing to output file...")
+    out_file.write(minimized_css_data)
+
+print("\nDone. Quiting in 3 secounds.")
+sleep(3)
+exit()
