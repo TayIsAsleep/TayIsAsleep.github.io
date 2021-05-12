305da3dcea318f5f0bd707a5ee22e531360cb027
TayIsAsleep <drpeperoni@outlook.com>
2021-05-12 10:35:10 +0200

    minor fixes

--- phasmo_tools/scss.py
+++ phasmo_tools/scss.py
@@ -2,8 +2,8 @@
 from requests import post
 from time import sleep
 
-with open("static/css/style.scss", "r", encoding="utf-8") as in_file, \
-     open("static/css/style.css", "w", encoding="utf-8") as out_file:
+with open("./static/css/style.scss", "r", encoding="utf-8") as in_file, \
+     open("./static/css/style.css", "w", encoding="utf-8") as out_file:
     
     print("Reading from .scss file...")
     scss_data = in_file.read()
