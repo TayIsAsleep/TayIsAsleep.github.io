d5bc93e72b8e2673a5c47c4c4f70291e71b83640
TayIsAsleep <drpeperoni@outlook.com>
2021-06-05 15:28:36 +0200

    yes

--- global/js/get_fresh.js
+++ global/js/get_fresh.js
@@ -1,14 +1,39 @@
-Array.from(document.querySelectorAll("link[href]")).concat(
-Array.from(document.querySelectorAll("script[src]")))
-.forEach(e => {
-    let attrs = e.getAttributeNames();
-    if (attrs.includes("ignore")){return;};
-
-    let t = "?t=" + Math.round(Date.now() / 1000);
-    if (attrs.includes("href")){
-        e.href += t;
-    }
-    else if (attrs.includes("src")){
-        e.src += t;
-    }
-});
+console.log("am ran first time");
+function get_fresh_run(){
+    console.log("am ran");
+    Array.from(document.querySelectorAll("link[href]")).concat(
+    Array.from(document.querySelectorAll("script[src]")))
+    .forEach(e => {
+        let attrs = e.getAttributeNames();
+        
+        if (attrs.includes("ignore")){return;};
+    
+        let t = "?t=" + Math.round(Date.now() / 1000);
+        let new_thing = document.createElement(e.tagName)
+        new_thing.setAttribute("ignore","1");
+    
+        attrs.forEach(a => {
+            let new_val = ""
+            if (a == "href"){
+                new_val = e.href += t;
+            }
+            else if (a == "src"){
+                new_val = e.src + t;
+            }
+            else{
+                new_val = eval("e." + a)
+            }
+            new_thing.setAttribute(a,new_val);
+        });
+    
+        console.log(new_thing);
+        document.head.appendChild(new_thing)
+        
+        // if (attrs.includes("href")){
+        //     e.href += t;
+        // }
+        // else if (attrs.includes("src")){
+        //     e.src += t;
+        // }
+    });
+}
---
+++ upload.py
@@ -0,0 +1,27 @@
+import re
+import random
+import os
+
+
+
+def hashMe(fn):
+    with open(fn,"r") as f:
+
+        in_ = f.read()
+
+        out = re.sub(
+            '(\?random=[0-9]{10}")',
+            f'?random={"".join(str(random.randint(0,9)) for x in range(10))}"',
+            in_
+        )
+    with open(fn,"w") as f:
+        f.write(out)
+        
+
+for path, subdirs, files in os.walk(os.getcwd()):
+    for name in files:
+        if name == "index.html":
+            this_file = os.path.join(path, name)
+            print(this_file)
+            hashMe(this_file)
+
