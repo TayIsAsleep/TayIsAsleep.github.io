0ba6853f16ac5b6465bc35e7276c3dd86d61076e
TayIsAsleep <drpeperoni@outlook.com>
2021-08-25 21:33:20 +0200

    daniel fat

--- apps/school/Webbutveckling 2/Produkt hemsida/script.js
+++ apps/school/Webbutveckling 2/Produkt hemsida/script.js
@@ -1,3 +1,15 @@
+var egg = 0;
 document.getElementsByClassName("menu-icon")[0].onclick = function(){
     window.location.href = "index.html"
 };
+
+function egg_check(me){
+    if (me.style.color == 'red'){return;};
+
+    me.style.color = 'red';
+    egg += 1;
+
+    if (egg == 6){
+        document.getElementsByClassName("egg1")[0].style.display = "block";
+    };
+}
