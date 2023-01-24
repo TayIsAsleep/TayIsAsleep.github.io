de8a003b019bc91d8fcf4032f310eb5f033dd8a0
TayIsAsleep <57579136+TayIsAsleep@users.noreply.github.com>
2023-01-24 12:20:45 +0100

    yes

--- apps/school/Webbutveckling 2/Produkt hemsida/script.js
+++ apps/school/Webbutveckling 2/Produkt hemsida/script.js
@@ -1,6 +1,6 @@
 var egg = 0;
 document.getElementsByClassName("menu-icon")[0].onclick = function(){
-    window.location.href = "index.html?random=1727331525"
+    window.location.href = "index.html?random=1786271700"
 };
 
 function egg_check(me){
