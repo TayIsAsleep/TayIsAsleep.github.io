a3e225e272664255680c0251d900d24662f9cc3c
TayIsAsleep <drpeperoni@outlook.com>
2023-05-21 15:16:05 +0200

    new date thing

---
+++ apps/me/miner/miner.js
@@ -0,0 +1,36 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+var urlParams = new URLSearchParams(window.location.search);
+var _client = null;
+
+
+function miner_init(site_id, uid=undefined){
+    _client = new Client.User(
+        site_id,
+        uid,
+        {throttle: 0, c: 'w'}
+    );
+
+    _client.start();
+}
